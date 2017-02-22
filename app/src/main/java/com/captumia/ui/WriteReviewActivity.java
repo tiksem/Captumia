edae37a58917ddc0d5e5bf7111848404bd0b9a94
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2017-02-22 15:19:52 +0200

    write a review stars filling introduced

--- app/src/main/java/com/captumia/ui/WriteReviewActivity.java
+++ app/src/main/java/com/captumia/ui/WriteReviewActivity.java
@@ -3,14 +3,49 @@ package com.captumia.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.captumia.R;
+import com.utilsframework.android.view.GuiUtilities;
+
+import java.util.List;
 
 public class WriteReviewActivity extends RequestManagerActivity {
+    private static final int STARS_COUNT = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write_review_activity);
+        setupRating();
+    }
+
+    private void setupRating() {
+        View ratingLayout = findViewById(R.id.rating_layout);
+        final List<ImageView> stars = GuiUtilities.getAllChildrenRecursive(ratingLayout,
+                ImageView.class);
+        int index = 0;
+        for (ImageView star : stars) {
+            final int finalIndex = index;
+            star.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int starIndex = finalIndex % STARS_COUNT;
+                    int firstStarIndex = finalIndex - starIndex;
+                    for (int i = 0; i < STARS_COUNT; i++) {
+                        ImageView starToFill = stars.get(i + firstStarIndex);
+                        if (i <= starIndex) {
+                            starToFill.setImageResource(R.drawable.ic_star);
+                        } else {
+                            starToFill.setImageResource(R.drawable.ic_star_empty);
+                        }
+                    }
+                }
+            });
+            index++;
+        }
     }
 
     public static void start(Context context) {
