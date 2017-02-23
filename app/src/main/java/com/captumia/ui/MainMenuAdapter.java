996e79155b292b3cf8841d199bd7c98b2ca55b59
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2017-02-24 01:42:19 +0200

    MainMenuAdapter.java improved

--- app/src/main/java/com/captumia/ui/MainMenuAdapter.java
+++ app/src/main/java/com/captumia/ui/MainMenuAdapter.java
@@ -2,11 +2,15 @@ package com.captumia.ui;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.captumia.R;
 import com.utilsframework.android.navdrawer.NavigationViewMenuAdapter;
 
 public class MainMenuAdapter extends NavigationViewMenuAdapter {
+    public static final int ITEM_SELECTION_DURATION = 500;
+
     private Context context;
 
     public MainMenuAdapter(Activity activity,
@@ -19,6 +23,15 @@ public class MainMenuAdapter extends NavigationViewMenuAdapter {
     @Override
     protected void onItemSelected(int itemId) {
         if (itemId == R.id.add_service) {
+            Menu menu = getNavigationMenuView().getMenu();
+            final MenuItem menuItem = menu.findItem(itemId);
+            menuItem.setChecked(true);
+            getNavigationMenuView().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    menuItem.setChecked(false);
+                }
+            }, ITEM_SELECTION_DURATION);
             AddPostActivity.start(context);
         } else {
             super.onItemSelected(itemId);
